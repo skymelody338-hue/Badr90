46e677114a10d00faaac1376faf56fb38b4a538b
skymelody338-hue <skymelody338@gmail.com>
2026-04-02 14:31:54 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("show");
+}
+
+
+window.onload = function() {
+  console.log("مرحباً بك في صفحة محافظة بدر التاريخية");
+}
